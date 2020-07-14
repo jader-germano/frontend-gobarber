d791392ff3134632f3c43f39bb4c71d974170f0f
jadergermano <jader.germano@icloud.com>
2020-07-14 01:26:59 -0300

    Adding Readme's for projects

--- src/pages/SignIn/sign-in.index.tsx
+++ src/pages/SignIn/sign-in.index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useContext } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLock, FiLogIn, FiMail } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
